c105798fa426ba4ab60f9fa5fafae7a4fa5bfd6c
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-16 19:30:09 +0100

    Making how to play

--- DungeonSlayer/src/HowToPlay/HowToPlay.jsx
+++ DungeonSlayer/src/HowToPlay/HowToPlay.jsx
@@ -1,6 +1,16 @@
+import styles from "./HowToPlay.module.css";
+
 function HowToPlay()
 {
-
+    return(
+        <>
+            <div className={styles.FullHowToPlay}>
+                <div>
+                    <h1>How to play?</h1>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default HowToPlay;
