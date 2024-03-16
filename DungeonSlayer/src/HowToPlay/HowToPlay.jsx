8288d80f18d189af424b72c17e4d1522bdd7ab06
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-16 20:19:16 +0100

    How to play is kinda done

--- DungeonSlayer/src/HowToPlay/HowToPlay.jsx
+++ DungeonSlayer/src/HowToPlay/HowToPlay.jsx
@@ -1,21 +1,22 @@
 import styles from "./HowToPlay.module.css";
 import PlayerImage from "../Sprites/Player.png";
 import EnemyImage from "../Sprites/Enemy.png";
+import { Link } from "react-router-dom";
 
 function HowToPlay()
 {
     return(
         <>
             <div className={styles.FullHowToPlay}>
-                <focus>
+                <main>
                     <h1 align="center">How to play?</h1>
                     <h2 align="center">Player</h2>
-                    <img src={PlayerImage} alt="Player" align="center"/>
+                    <img src={PlayerImage} alt="Player" align="center" className={styles.imgGuide}/>
                     <div>
                         <h3 align="center">
                             Movement   
                         </h3><br/>
-                        <span>
+                        <span className={styles.GuideText}>
                             Use the arrow keys or WASD to move your character.
                         </span>
                     </div>
@@ -23,16 +24,21 @@ function HowToPlay()
                         <h3 align="center">
                             Attack
                         </h3><br/>
-                        <span>
+                        <span className={styles.GuideText}>
                             Press k to use your sword to attack
                         </span>
                     </div>
                     <h2 align="center">Enemies</h2>
-                    <img src={EnemyImage} alt="Enemy"/><br/>
-                    <span>
+                    <img src={EnemyImage} alt="Enemy" className={styles.imgGuide}/><br/>
+                    <span className={styles.GuideText}>
                         Kill the enemy before they kill you!
                     </span>
-                </focus>
+                </main>
+                <div align="center">
+                    <Link to="/">
+                        <button className={styles.btnMenu}>Menu</button>
+                    </Link>
+                </div>
             </div>
         </>
     );
--- DungeonSlayer/src/Menu/Menu.jsx
+++ DungeonSlayer/src/Menu/Menu.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { Link, BrowserRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "./Menu.module.css";
-import Game from "../Game/Game";
-import HowToPlay from "../HowToPlay/HowToPlay"
 
 function Menu() {
   return (
+    <>
       <div className={styles.MenuDiv}>
-        <h1>Menu</h1>
-        <br />
-        <Link to="/Game">
-          <button>play</button>
-        </Link>
-        <br />
-        <Link to="/HowToPlay">
-          <button>how to play</button>
-        </Link>
-        <br />
+        <h1>Menu</h1><br/>
       </div>
+      <div className={styles.NavMenu}>
+            <Link to="/Game">
+            <button className={styles.btnMenu}>play</button>
+            </Link>
+            <br />
+            <Link to="/HowToPlay">
+            <button className={styles.btnMenu}>how to play</button>
+            </Link>
+      </div>
+    </>
   );
 }
 
