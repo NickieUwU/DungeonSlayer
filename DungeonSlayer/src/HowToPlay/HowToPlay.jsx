6b26756b79ad750b84b79f1cfb0a51be65ef2ec5
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-16 20:06:02 +0100

    How to play is almost done

--- DungeonSlayer/src/HowToPlay/HowToPlay.jsx
+++ DungeonSlayer/src/HowToPlay/HowToPlay.jsx
@@ -1,4 +1,6 @@
 import styles from "./HowToPlay.module.css";
+import PlayerImage from "../Sprites/Player.png";
+import EnemyImage from "../Sprites/Enemy.png";
 
 function HowToPlay()
 {
@@ -7,14 +9,29 @@ function HowToPlay()
             <div className={styles.FullHowToPlay}>
                 <focus>
                     <h1 align="center">How to play?</h1>
-                    <h2 align="Center">Player</h2>
-                    <img src="../Sprites/Player.png" alt="Player"/>
+                    <h2 align="center">Player</h2>
+                    <img src={PlayerImage} alt="Player" align="center"/>
                     <div>
                         <h3 align="center">
                             Movement   
                         </h3><br/>
-                        Use the arrow keys or WASD to move your character.
+                        <span>
+                            Use the arrow keys or WASD to move your character.
+                        </span>
                     </div>
+                    <div>
+                        <h3 align="center">
+                            Attack
+                        </h3><br/>
+                        <span>
+                            Press k to use your sword to attack
+                        </span>
+                    </div>
+                    <h2 align="center">Enemies</h2>
+                    <img src={EnemyImage} alt="Enemy"/><br/>
+                    <span>
+                        Kill the enemy before they kill you!
+                    </span>
                 </focus>
             </div>
         </>
