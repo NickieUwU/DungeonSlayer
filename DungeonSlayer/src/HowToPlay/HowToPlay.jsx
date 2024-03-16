501cc6ac460fd413641d64b01616c826ec3de04d
VlastimilVasut <vasutvlastimil@gmail.com>
2024-03-16 19:44:10 +0100

    Made a player sprite

--- DungeonSlayer/src/HowToPlay/HowToPlay.jsx
+++ DungeonSlayer/src/HowToPlay/HowToPlay.jsx
@@ -5,9 +5,17 @@ function HowToPlay()
     return(
         <>
             <div className={styles.FullHowToPlay}>
-                <div>
-                    <h1>How to play?</h1>
-                </div>
+                <focus>
+                    <h1 align="center">How to play?</h1>
+                    <h2 align="Center">Player</h2>
+                    <img src="../Sprites/Player.png" alt="Player"/>
+                    <div>
+                        <h3 align="center">
+                            Movement   
+                        </h3><br/>
+                        Use the arrow keys or WASD to move your character.
+                    </div>
+                </focus>
             </div>
         </>
     );
